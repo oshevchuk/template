a7468399128dd5d29825359d6ccd29af6b3b61fc
User <megadevice83@gmail.com>
2017-05-12 22:47:48 +0300

    init

---
+++ Gulpfile.js
@@ -0,0 +1,17 @@
+/**
+ * Created by User on 12.05.2017.
+ */
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
