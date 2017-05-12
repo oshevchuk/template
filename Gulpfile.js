b65c991769898a351fa9ba3b4413d63bfa5a8ed9
User <megadevice83@gmail.com>
2017-05-12 23:52:11 +0300

    init

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,6 +6,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+
 gulp.task('sass', function () {
     gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
@@ -15,3 +16,4 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
 });
+
